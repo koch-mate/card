9573ea94ce85ef59e91e01c70a3131079f0a661d
Mate Koch <mkoch@cae-engineering.hu>
2021-03-13 01:03:17 +0100

    Cleanup, docs

--- grid.php
+++ grid.php
@@ -1,7 +1,18 @@
 <?php
 
+// number of cards in the game
 $CARDS_NUM = 40;
 
+//  directory structure:
+//
+//    cards/
+//      big/            <- side A full size
+//      big-back/       <- side B full size
+//      thumb/          <- size A thumbnail
+//      cover.jpg
+//
+//  all pictures in "ImageNN.jpg" format, NN goes from 01 to $CARDS_NUM with
+//  zero padding to two digits
 ?>
 <!doctype html>
 <html lang="en">
@@ -9,31 +20,31 @@ $CARDS_NUM = 40;
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Card game sample</title>
-
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="grid.css" rel="stylesheet">
     <script src="js/jquery-3.6.0.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
   </head>
+
   <body class="py-4">
 
 <main>
   <div id="welcome" class="modal" tabindex="-1">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Card game title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <p>Hey, this is a game.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Card game title</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <p>Hey, this is a game.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
 
   <div class="container">
     <div class="row mb-3">
@@ -44,7 +55,6 @@ $CARDS_NUM = 40;
           foreach($nums as $i){ ?>
           <button class="thumb" data-rel="Image<?php printf('%02d', $i+1)?>.jpg">
             <img src="cards/thumb/Image<?php printf('%02d', $i)?>.jpg">
-
           </button>
           <?php } ?>
       </div>
@@ -57,18 +67,17 @@ $CARDS_NUM = 40;
 </main>
 
 <script>
-$(".thumb").click(function () {
-    var imgUrl = $(this).data('rel');
-    $("#big-card").html("<img src='cards/big/" + imgUrl + "'  />");
-    $("#big-card").click({imgUrl : imgUrl}, flipCard);
-    function flipCard(event) {
-        $("#big-card").html("<img src='cards/big-back/" + event.data.imgUrl + "'  />");
-      }
-
-});
-$(window).on('load', function() {
-        $('#welcome').modal('show');
-});
+  $(".thumb").click(function () {
+      var imgUrl = $(this).data('rel');
+      $("#big-card").html("<img src='cards/big/" + imgUrl + "'  />");
+      $("#big-card").click({imgUrl : imgUrl}, flipCard);
+      function flipCard(event) {
+          $("#big-card").html("<img src='cards/big-back/" + event.data.imgUrl + "'  />");
+        }
+  });
+  $(window).on('load', function() {
+      $('#welcome').modal('show');
+  });
 </script>
 
 
